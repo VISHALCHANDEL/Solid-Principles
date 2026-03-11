fda8db8f66a9919d599890bf2b00e7853d7e0b1f
Vishal Chandel <119956220+VISHALCHANDEL@users.noreply.github.com>
2026-03-11 19:36:30 +0530

    Delete 4.Solid Principles directory

--- 4.Solid Principles/03-March-2025(SRP)/Solid/Example1/BetterCode/EmployeePerformanceReportGenerator.java
+++
@@ -1,8 +0,0 @@
-package BetterCode;
-
-public class EmployeePerformanceReportGenerator {
-
-    public void printPerformanceReport(Employee employee){
-        System.out.println("Performance report for employee: " + employee.getEmployeeName());
-    }
-}
--- 4.Solid Principles/03-March-2025(SRP)/Solid/Example2/BetterCode/ActivityLogger.java
+++
@@ -1,7 +0,0 @@
-package Example2.BetterCode;
-
-public class ActivityLogger {
-    public void logUserAcitvity(User user){
-        System.out.println("User activity logged");
-    }
-}
